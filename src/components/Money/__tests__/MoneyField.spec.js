4674118aca6d971d0a7cf7671ce9efddc72bfe6c
Merisho <merishot@gmail.com>
2019-08-20 13:21:56 +0300

    Add more unit tests for MoneyField

--- src/components/Money/__tests__/MoneyField.spec.js
+++ src/components/Money/__tests__/MoneyField.spec.js
@@ -4,7 +4,21 @@ import { mount } from 'enzyme';
 import MoneyField from '../MoneyField';
 
 describe('Money input field representation component', () => {
-  test('Formats value with number of digits <= 3', () => {
+  test('Must set the value as 0 by default', () => {
+    const wrapper = mount(<MoneyField />);
+    const input = wrapper.find('input');
+
+    expect(input.instance().value).toBe('0');
+  });
+
+  test('Must set default value if it is specified', () => {
+    const wrapper = mount(<MoneyField defaultValue="100" />);
+    const input = wrapper.find('input');
+
+    expect(input.instance().value).toBe('100');
+  });
+
+  test('Must format value with number of digits <= 3', () => {
     const wrapper = mount(<MoneyField />);
     const input = wrapper.find('input');
 
@@ -13,7 +27,7 @@ describe('Money input field representation component', () => {
     expect(input.instance().value).toBe('999');
   });
 
-  test('Formats value with number of digits >= 4 and <= 6', () => {
+  test('Must format value with number of digits >= 4 and <= 6', () => {
     const wrapper = mount(<MoneyField />);
     const input = wrapper.find('input');
 
@@ -22,12 +36,41 @@ describe('Money input field representation component', () => {
     expect(input.instance().value).toBe('99,999');
   });
 
-  test('Formats value with number of digits >= 7', () => {
+  test('Must format value with number of digits >= 7', () => {
     const wrapper = mount(<MoneyField />);
     const input = wrapper.find('input');
 
     input.simulate('change', { target: { value: '1500000' } });
 
     expect(input.instance().value).toBe('1,500,000');
+  });
+
+  test('Must not change value at all in case string is typed in', () => {
+    const wrapper = mount(<MoneyField />);
+    const input = wrapper.find('input');
+
+    input.simulate('change', { target: { value: 'test' } });
+
+    expect(input.instance().value).toBe('0');
+  });
+
+  test('Must change value to a single 0 only in case multiple 0s typed in', () => {
+    const wrapper = mount(<MoneyField />);
+    const input = wrapper.find('input');
+
+    input.simulate('change', { target: { value: '0' } });
+    input.simulate('change', { target: { value: '0' } });
+
+    expect(input.instance().value).toBe('0');
+  });
+
+  test('Must trigger onChange handler', () => {
+    const changeHandler = jest.fn();
+    const wrapper = mount(<MoneyField onChange={changeHandler} />);
+    const input = wrapper.find('input');
+
+    input.simulate('change', { target: { value: '1' } });
+
+    expect(changeHandler).toHaveBeenCalled();
   });
 });
